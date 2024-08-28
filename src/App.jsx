44ebf06e1e6a3f81c965b7d3f53b94582ffb8305
unbrokendude <youssefbenslimane00@gmail.com>
2024-08-28 23:03:12 +0100

    use hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/MuiNavbar";
 import Hero from "./Components/Hero/Hero";
 import Services from "./Components/Service/Service";
