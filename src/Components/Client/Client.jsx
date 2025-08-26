bb5333c81272bd44af2526553ae5228929fcb72d
youssef-benslimane <youssef.benslimane@innovex-consulting.fr>
2025-08-26 14:57:39 +0100

    - Ajout des nouveaux clients : GSK, C&A, AirFrance
    - Ajout d'un nouveau consultant
    - Ajout d'un nouveau offre de stage FullStack
    - Modification des chiffres

--- src/Components/Client/Client.jsx
+++ src/Components/Client/Client.jsx
@@ -4,27 +4,45 @@ import vinci from "../../assets/vinci.png";
 import luxottica from "../../assets/luxottica.png";
 import groupePomona from "../../assets/group-pomona.png";
 import esteeLauder from "../../assets/estee-lauder.png";
+import gsk from "../../assets/gsk.png";
+import ca from "../../assets/ca.png";
+import airFrance from "../../assets/Air-France.png";
 import Grid from "@mui/material/Grid2";
 import { Box } from "@mui/material";
 
 const ClientDetails = [
   {
     id: 1,
+    name: "GSK",
+    image: gsk,
+  },
+  {
+    id: 2,
+    name: "C&A",
+    image: ca,
+  },
+  {
+    id: 3,
+    name: "Air France",
+    image: airFrance,
+  },
+  {
+    id: 4,
     name: "Vinci Construction",
     image: vinci,
   },
   {
-    id: 2,
+    id: 5,
     name: "Luxottica",
     image: luxottica,
   },
   {
-    id: 3,
+    id: 6,
     name: "Groupe Pomona",
     image: groupePomona,
   },
   {
-    id: 4,
+    id: 7,
     name: "Estee Lauder Companies",
     image: esteeLauder,
   },
